c7c94391a55629d22d54f447ade19254438655f2
Tridib11 <tridibghosh12345@gmail.com>
2024-03-24 04:56:07 +0530

    File added

--- 2-create-react-comp/src/App.jsx
+++ 2-create-react-comp/src/App.jsx
@@ -1,8 +1,10 @@
+import { Hello } from "./Hello";
 import KgButton from "./KgButton";
 function App() {
   return <div>
     <h1>this is the best react course</h1>
     <KgButton/>
+    <Hello/>
   </div>
 }
 
