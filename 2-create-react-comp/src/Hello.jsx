3a606aac0aa0f1faee54d40c6b618d84090c577c
Tridib11 <tridibghosh12345@gmail.com>
2024-03-24 04:59:23 +0530

    File added

--- 2-create-react-comp/src/Hello.jsx
+++ 2-create-react-comp/src/Hello.jsx
@@ -1,6 +1,9 @@
 export function Hello(){
   let myName="Tridib"
+  let fullName=()=>{
+    return "Tridib"
+  }
   return <h3>
-    Hello this is {myName}
+    Hello this is {fullName()}
   </h3>
 }
