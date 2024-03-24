5d3d3d8731dddda6cd4457b1e6afb07fbf11bba5
Tridib11 <tridibghosh12345@gmail.com>
2024-03-24 15:38:52 +0530

    File added

--- projects/Todo-app/src/components/AddTodo.jsx
+++ projects/Todo-app/src/components/AddTodo.jsx
@@ -8,7 +8,7 @@ export function AddTodo() {
         <input type="date" name="" id="" />
       </div>
       <div class="col-2">
-        <button type="button" class="btn btn-success">
+        <button type="button" class="btn btn-success kg-button">
           Add
         </button>
       </div>
