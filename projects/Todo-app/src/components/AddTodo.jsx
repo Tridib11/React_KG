513247ea39976113af8198827ee0b57741759e36
Tridib11 <tridibghosh12345@gmail.com>
2024-03-24 15:19:31 +0530

    File added

---
+++ projects/Todo-app/src/components/AddTodo.jsx
@@ -0,0 +1,17 @@
+export function AddTodo() {
+  return (
+    <div class="row">
+      <div class="col-4">
+        <input type="text" name="" id="" placeholder="Enter todo here" />
+      </div>
+      <div class="col-4">
+        <input type="date" name="" id="" />
+      </div>
+      <div class="col-2">
+        <button type="button" class="btn btn-success">
+          Add
+        </button>
+      </div>
+    </div>
+  );
+}
