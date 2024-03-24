a3e1a350646df0845fddbe740f148d6a39fab1cc
Tridib11 <tridibghosh12345@gmail.com>
2024-03-24 15:38:55 +0530

    File added

--- projects/Todo-app/src/components/Todos.jsx
+++ projects/Todo-app/src/components/Todos.jsx
@@ -1,11 +1,11 @@
 export function Todos({title,date}) {
   return (
-    <div>
-      <div class="row">
+    <div className="kg-row">
+      <div className="row">
         <div class="col-4">{title}</div>
         <div class="col-4">{date}</div>
         <div class="col-2">
-          <button type="button" class="btn btn-danger">
+          <button type="button" class="btn btn-danger kg-button">
             Delete
           </button>
         </div>
