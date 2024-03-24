a31d51b37700ef52e6fef0103ecf82903125e737
Tridib11 <tridibghosh12345@gmail.com>
2024-03-25 00:41:28 +0530

    File added

--- projects/Calculator/src/App.jsx
+++ projects/Calculator/src/App.jsx
@@ -1,9 +1,17 @@
+import styles from "./App.module.css"
 function App() {
   
   return (
-    <>
-     
-    </>
+    <div className={styles.calculator}>
+      <input className={styles.display} type="text" id="display" />
+      <div className={styles.buttonsContainer}>
+        <button className={styles.button}>C</button>
+        <button className={styles.button}>1</button>
+        <button className={styles.button}>2</button>
+        <button className={styles.button}>3</button>
+        <button className={styles.button}>4</button>
+      </div>
+    </div>
   )
 }
 
