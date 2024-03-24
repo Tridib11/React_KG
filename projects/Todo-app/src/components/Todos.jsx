6e8988b8b03e0a1c52a3c171e19354a566f801c8
Tridib11 <tridibghosh12345@gmail.com>
2024-03-24 15:19:33 +0530

    File added

---
+++ projects/Todo-app/src/components/Todos.jsx
@@ -0,0 +1,25 @@
+export function Todos() {
+  return (
+    <div>
+      <div class="row">
+        <div class="col-4">Buy Milk</div>
+        <div class="col-4">4/10/2023</div>
+        <div class="col-2">
+          <button type="button" class="btn btn-danger">
+            Delete
+          </button>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-4">Goto college</div>
+        <div class="col-4">4/10/2023</div>
+        <div class="col-2">
+          <button type="button" class="btn btn-danger">
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
