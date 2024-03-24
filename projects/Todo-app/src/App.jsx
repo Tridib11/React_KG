282dce33fabdae31cb1eeae8b9c8d02969ae9b0a
Tridib11 <tridibghosh12345@gmail.com>
2024-03-24 15:24:04 +0530

    File added

--- projects/Todo-app/src/App.jsx
+++ projects/Todo-app/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <div class="container text-center">
         <AddTodo/>
         <Todos title={"go to school"} date={"10/1/2024"}/>   
-        <Todos title={"go to gym"} date={"10/1/2025"}/>      
+        <Todos title={"go to gym"} date={"10/1/202"}/>      
       </div>
     </center>
   );
