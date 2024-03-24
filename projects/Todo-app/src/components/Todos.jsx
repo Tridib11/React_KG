72a51abbc3e639e2de10b7f6f72df4eed3bedf65
Tridib11 <tridibghosh12345@gmail.com>
2024-03-24 15:23:59 +0530

    File added

--- projects/Todo-app/src/components/Todos.jsx
+++ projects/Todo-app/src/components/Todos.jsx
@@ -1,19 +1,9 @@
-export function Todos() {
+export function Todos({title,date}) {
   return (
     <div>
       <div class="row">
-        <div class="col-4">Buy Milk</div>
-        <div class="col-4">4/10/2023</div>
-        <div class="col-2">
-          <button type="button" class="btn btn-danger">
-            Delete
-          </button>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="col-4">Goto college</div>
-        <div class="col-4">4/10/2023</div>
+        <div class="col-4">{title}</div>
+        <div class="col-4">{date}</div>
         <div class="col-2">
           <button type="button" class="btn btn-danger">
             Delete
