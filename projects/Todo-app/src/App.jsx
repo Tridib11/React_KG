3a648bfe925be5135eb808994d7b7aa83b3ad721
Tridib11 <tridibghosh12345@gmail.com>
2024-03-24 15:38:49 +0530

    File added

--- projects/Todo-app/src/App.jsx
+++ projects/Todo-app/src/App.jsx
@@ -1,16 +1,18 @@
 import { AddTodo } from "./components/AddTodo";
 import { AppName } from "./components/AppName";
 import { Todos } from "./components/Todos";
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
-    <center class="todo-container">
-      <AppName/>
-      <div class="container text-center">
-        <AddTodo/>
-        <Todos title={"go to school"} date={"10/1/2024"}/>   
-        <Todos title={"go to gym"} date={"10/1/202"}/>      
+    <center className="todo-container">
+      <AppName />
+      <div className="container">
+        <div className="items-container">
+          <AddTodo />
+          <Todos title={"go to school"} date={"10/1/2024"} />
+          <Todos title={"go to gym"} date={"10/1/202"} />
+        </div>
       </div>
     </center>
   );
