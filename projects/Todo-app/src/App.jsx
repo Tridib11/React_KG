21b8d2f94d19262bd50ccb15c996cd68f46eb5b2
Tridib11 <tridibghosh12345@gmail.com>
2024-03-24 15:15:02 +0530

    File added

--- projects/Todo-app/src/App.jsx
+++ projects/Todo-app/src/App.jsx
@@ -1,9 +1,46 @@
+import { AppName } from "./components/AppName";
+
 function App() {
-  return <center class='todo-container'>
-   <h1>Todo app</h1>
-  
-  </center>
- 
+  return (
+    <center class="todo-container">
+      <AppName/>
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-4">
+            <input type="text" name="" id="" placeholder="Enter todo here" />
+          </div>
+          <div class="col-4">
+            <input type="date" name="" id="" />
+          </div>
+          <div class="col-2">
+            <button type="button" class="btn btn-success">
+              Add
+            </button>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-4">Buy Milk</div>
+          <div class="col-4">4/10/2023</div>
+          <div class="col-2">
+            <button type="button" class="btn btn-danger">
+              Delete
+            </button>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-4">Goto college</div>
+          <div class="col-4">4/10/2023</div>
+          <div class="col-2">
+            <button type="button" class="btn btn-danger">
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
+    </center>
+  );
 }
 
-export default App
+export default App;
