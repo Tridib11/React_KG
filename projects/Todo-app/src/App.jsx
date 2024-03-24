132dc05cda0fca674b97069124ae7f56ae712c72
Tridib11 <tridibghosh12345@gmail.com>
2024-03-24 15:19:28 +0530

    File added

--- projects/Todo-app/src/App.jsx
+++ projects/Todo-app/src/App.jsx
@@ -1,43 +1,14 @@
+import { AddTodo } from "./components/AddTodo";
 import { AppName } from "./components/AppName";
+import { Todos } from "./components/Todos";
 
 function App() {
   return (
     <center class="todo-container">
       <AppName/>
       <div class="container text-center">
-        <div class="row">
-          <div class="col-4">
-            <input type="text" name="" id="" placeholder="Enter todo here" />
-          </div>
-          <div class="col-4">
-            <input type="date" name="" id="" />
-          </div>
-          <div class="col-2">
-            <button type="button" class="btn btn-success">
-              Add
-            </button>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-4">Buy Milk</div>
-          <div class="col-4">4/10/2023</div>
-          <div class="col-2">
-            <button type="button" class="btn btn-danger">
-              Delete
-            </button>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-4">Goto college</div>
-          <div class="col-4">4/10/2023</div>
-          <div class="col-2">
-            <button type="button" class="btn btn-danger">
-              Delete
-            </button>
-          </div>
-        </div>
+        <AddTodo/>
+        <Todos/>      
       </div>
     </center>
   );
